763ec01be2801f476cba8757a4044ce797c4dbc7
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-11 14:28:14 +0300

    chore: setup springboot project

---
+++ src/test/java/org/booking/Prog3SpringbootBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3SpringbootBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
